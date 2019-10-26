3549bfe9924380401c6e6151112445480bbf2e8f
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2019-10-26 00:24:28 -0700

    Recommendation Engine 1.0
    Shows the highest rated books.

--- recommendationEngine.py
+++ recommendationEngine.py
@@ -4,6 +4,7 @@
 #importing libs
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import warnings
 warnings.simplefilter('ignore')
 
@@ -15,8 +16,33 @@ df = pd.read_csv('books.csv', sep='\s*,\s*',error_bad_lines=False)
 
 #for the shape of the dataset
 #print(df.shape)
+#size -- (13719, 8)
 
 #dropping unnamed columns
 df=df.loc[:,~df.columns.str.contains('^Unnamed')]
 
-print(df.head())
+#dropping isbn and isbn13 columns
+df.drop(['isbn','isbn13'],axis=1,inplace=True)
+#print(df.columns)
+
+#display all the datatypes
+#print(df.dtypes)
+
+
+#will help to show all the text strings in the column.
+pd.set_option('display.max_colwidth',-1)
+
+
+
+#simple popularity based recommendation
+top10 = df.sort_values('average_rating',ascending = False).head(5)
+
+
+ 
+
+
+
+
+
+
+
